50e3a222f53f123921ff4fbcac595150ca9468b2
theoalll <theo.ananda@ui.ac.id>
2024-09-25 13:41:29 +0700

    add function for delete mood

--- main/urls.py
+++ main/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('edit-mood/<uuid:id>', edit_mood, name='edit_mood'),
+    path('delete/<uuid:id>', delete_mood, name='delete_mood'), 
+
 ]
--- main/views.py
+++ main/views.py
@@ -123,3 +123,12 @@ def edit_mood(request, id):
 
     context = {'form': form}
     return render(request, "edit_mood.html", context)
+
+# Menambahkan Fitur Hapus Mood pada Aplikasi
+def delete_mood(request, id):
+    # Get mood berdasarkan id
+    mood = MoodEntry.objects.get(pk = id)
+    # Hapus mood
+    mood.delete()
+    # Kembali ke halaman awal
+    return HttpResponseRedirect(reverse('main:show_main'))
